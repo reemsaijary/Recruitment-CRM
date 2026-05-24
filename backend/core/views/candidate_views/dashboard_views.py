170c7e748b1d03d516d0974a4be784f06560acb8
Reem Saijary <reemsaijary31@gmail.com>
2026-05-24 12:28:56 +0300

    Refactor company and candidate dashboards using role decorators

--- backend/core/views/candidate_views/dashboard_views.py
+++ backend/core/views/candidate_views/dashboard_views.py
@@ -1,11 +1,7 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import redirect
+from core.decorators import role_required
 
-@login_required
+
+@role_required(['candidate'])
 def candidate_dashboard(request):
-
-    if request.user.profile.role != 'candidate':
-        return redirect('login')
-
     return render(request, 'core/candidate_dashboard/dashboard.html')
--- backend/core/views/company_views/dashboard_views.py
+++ backend/core/views/company_views/dashboard_views.py
@@ -1,11 +1,7 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import redirect
+from core.decorators import role_required
 
-@login_required
+
+@role_required(['company'])
 def company_dashboard(request):
-
-    if request.user.profile.role != 'company':
-        return redirect('login')
-
     return render(request, 'core/company_dashboard/dashboard.html')
